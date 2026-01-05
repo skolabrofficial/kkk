2d11ee307dfa0249546564de4fcec04b36623737
skolabrofficial <modcrafttestovka@wo.cz>
2026-01-05 19:50:39 +0100

    Initialize Supabase client in supabaseServer.ts

---
+++ lib/supabaseServer.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabaseServer = createClient(url, serviceKey);
